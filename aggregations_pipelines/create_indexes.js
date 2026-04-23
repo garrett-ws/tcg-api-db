db8e61c387fcb0d21e55dcd439d1dae2169c7666
garrett-ws <garrettwsangala@gmail.com>
2026-04-23 10:58:54 -0400

    Removed unused indexes from create_indexes (mostly legacy indexes)

--- aggregations_pipelines/create_indexes.js
+++ aggregations_pipelines/create_indexes.js
@@ -8,25 +8,6 @@
  * db.prices
  */
 
-// This index is for the older version of the pipeline
-// If the pipeline is successfully updated then this can be dropped
-db.prices.createIndex(  
-  { "tcg.normal.low": 1, "set_id": -1, "product_id": 1 },
-  { name: "idx_prices_legacy_pipeline" }
-)
-
-// Used for an in progress version of the pipeline, may not end up
-// being used
-db.prices.createIndex(
-  { "date_updated": -1, "product_id": 1 },
-  { name: "idx_prices_date_product" }
-)
-
-db.prices.createIndex(
-  { "high_price": -1 },
-  { name: "idx_prices_high_price" }
-)
-
 db.prices.createIndex(
   { "tcg_high_price": -1 },
   { name: "idx_prices_tcg_high_price" }
@@ -35,25 +16,6 @@ db.prices.createIndex(
 /**
  * db.prices_current
  */
-
-// This index is for the older version of the pipeline
-// If the pipeline is successfully updated then this can be dropped
-db.prices_current.createIndex(  
-  { "tcg.normal.low": 1, "set_id": -1, "product_id": 1 },
-  { name: "idx_prices_legacy_pipeline" }
-)
-
-// Used for an in progress version of the pipeline, may not end up
-// being used
-db.prices_current.createIndex(
-  { "date_updated": -1, "product_id": 1 },
-  { name: "idx_prices_date_product" }
-)
-
-db.prices_current.createIndex(
-  { "high_price": -1 },
-  { name: "idx_prices_high_price" }
-)
 
 db.prices_current.createIndex(
   { "tcg_high_price": -1 },
